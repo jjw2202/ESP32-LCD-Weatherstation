633e2f77396ce1ed1506909b6460aebc328269f8
jjw2202 <jjw2202@online.de>
2020-03-26 17:17:15 +0100

    optimize weatherloop()

--- weather.ino
+++ weather.ino
@@ -11,33 +11,30 @@ void weatherloop() {
   switch (screencount) {
     case 0: //city, country | weather description
       screentext[0] = weather.cityname + ", " + weather.country;
-      lcdprint(0, screentext[0]);
       screentext[1] = weather.longdescription;
-      lcdprint(1, screentext[1]);
-      screencount++;
       break;
     case 1: //temperature | humidity
       screentext[0] = String("Temp.: ") + String(weather.temperature) + "\337C";
-      lcdprint(0, screentext[0]);
       screentext[1] = String("Humidity: ") + String(weather.humidity) + "%";
-      lcdprint(1, screentext[1]);
-      screencount++;
       break;
     case 2: //wind | clouds
       screentext[0] = String("Wind: ") + String(weather.wind) + "m/s";
-      lcdprint(0, screentext[0]);
       screentext[1] = String("Clouds: ") + String(weather.cloud) + "%";
-      lcdprint(1, screentext[1]);
-      screencount++;
       break;
     case 3: //rain | snow
       screentext[0] = String("Rain: ") + String(weather.rain) + "mm/h";
-      lcdprint(0, screentext[0]);
-      screentext[1] = String("Snow: ") + String(weather.snow) + "mm/h";
-      lcdprint(1, screentext[1]);
-      screencount++;
+      if (weather.snow != 0) {
+        screentext[1] = String("Snow: ") + String(weather.snow) + "mm/h";
+      }
+      else {
+        screentext[1] = "";
+      }
       break;
   }
+  for (uint8_t i = 0; i < 2; i++) {
+    lcdprint(i, screentext[i]);
+  }
+  screencount++;
 }
 
 void updateweather() {
